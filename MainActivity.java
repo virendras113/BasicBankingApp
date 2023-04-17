1edfe4101fff9c18262c2b7b1097238a57118f8d
Anand Krishn Mishra <92173758+Y-SURGE-Y@users.noreply.github.com>
2023-04-17 23:15:27 +0530

    Some Minute Changes

--- MainActivity.java
+++ MainActivity.java
@@ -1,23 +1,19 @@
 package com.chaitanya.bankingapp;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
-
+    Scanner sc =new Scanner(System.in);
     Button view_users;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         view_users = findViewById(R.id.view_users);
-
         view_users.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
